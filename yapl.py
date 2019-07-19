3869289b6d282efd677c70be26d5363ecd3565f0
dschaedler <dominicschaedler@gmail.com>
2019-07-18 20:52:57 -0400

    cmpdl and file detection

---
+++ yapl.py
@@ -0,0 +1,11 @@
+#Yet Another Pack Launcher
+#Dee Schaedler
+
+#Let's not lose the source this time
+
+#Imports
+import os.path
+from os import path
+
+cmpdl_test = path.exists('cmpdl-1.2.jar')
+print("cmpdl present: " + str(cmpdl_test))
